96f2b3e7d522d6fcc6d9c613a81392b749e0e100
Shivank254 <71582134+Shivank254@users.noreply.github.com>
2020-11-01 11:45:59 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -24,10 +24,34 @@ function logout(){
       localStorage.removeItem("User");
 }
 
+function add_room_fn(){
+      room_name = document.getElementById("roomname").value;
+      if(room_name == ""){
+            document.getElementById("error").innerHTML = "Please mention a roomname to start chating";
+            setTimeout(function(){document.getElementById("error").innerHTML = "";}, 2000);
+      }
+      else{
+            firebase.database().ref("/").child(room_name).update({
+                  purpose:"adding room name"
+            });
+            localStorage.setItem("room_name", room_name);
+            window.location = "kwitter_page.html";
+            document.getElementById("roomname").innerHTML = "";
+      }
+}
+
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
-
+      console.log("Roomname - " + Room_names);
+      row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)'>#" + Room_names + "</div><hr>";
+      document.getElementById("output").innerHTML += row;
       //End code
       });});}
 getData();
+
+function redirectToRoomName(name){
+      console.log(name);
+      localStorage.setItem("room_name_user_present", name);
+      window.location = "kwitter_page.html";
+}
